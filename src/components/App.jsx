81830d93b653306f3c87464b597fd76874ac95ae
Orin Fletcher <orin.fletcher@gmail.com>
2021-01-11 14:35:48 -0800

    updates with useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-function App() {
-  return <div />;
-}
+
+
+function App(){
+  const state = useState(0);
+ 
+    
+  function inc(){
+    // count++;
+  }
+
+  return (
+    <div className="container">
+      <h1>{state[0]}</h1>
+      <button onClick={inc}>+</button>
+    </div>
+  )
+};
 
 export default App;
